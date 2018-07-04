c5e8a1b42b98a0305d1bfec98b5e6835db301f3c
jibai <1439125361@qq.com>
2018-07-04 16:08:05 +0800

    Initial Commit

---
+++ EAIntroView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EAIntroView
+//
+//  Created by 郭嘉 on 2018/7/4.
+//  Copyright © 2018年 郭嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EAIntroView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EAIntroView
+//
+//  Created by 郭嘉 on 2018/7/4.
+//  Copyright © 2018年 郭嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
